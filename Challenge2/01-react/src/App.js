d8e19ed095f124591efb4d03c43bdeaa6e069a38
JuanMurcia1 <124263617+JuanMurcia1@users.noreply.github.com>
2023-09-10 12:26:32 -0500

    Add files via upload

---
+++ Challenge2/01-react/src/App.js
@@ -0,0 +1,10 @@
+function App(){
+  return (
+    <>
+    <h1> Hola Mundo</h1>
+    <h2> Hola de qué pedazo de zunga</h2>
+    </>
+  
+  )
+}
+export default App;
---
+++ Challenge2/01-react/src/FirstApp.js
@@ -0,0 +1,10 @@
+function Textos(){
+    return (
+      <>
+      <h1> Pandora</h1>
+      <span> 10</span>
+      </>
+    
+    )
+  }
+  export default Textos;
